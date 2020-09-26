4c294acb0d65ebe3a1c6ff00d2775b7669877aa8
Toktam <ttmohebbi@gmail.com>
2020-09-26 03:28:31 +0000

    minor fix update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Arrays to keep track of each task's state
-const taskTitles = [];
-const taskComplete = [];
-const taskDescriptions = [];
+// const taskTitles = [];
+// const taskComplete = [];
+// const taskDescriptions = [];
 
 // Create a new task by adding to the arrays
 // A new task will be created as incomplete
